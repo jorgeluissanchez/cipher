ee74f89cbed2a239e856f2a07ddc8f79aeec7470
jorge sanchez <jorgeluissanchezbarreneche@gmail.com>
2024-03-31 11:11:17 -0500

    Fix width

--- src/components/encode-form.tsx
+++ src/components/encode-form.tsx
@@ -70,7 +70,7 @@ export function EncodeForm() {
       toast({
         title: "Haz enviado los siguientes valores:",
         description: (
-          <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <pre className="mt-2 w-[300px] md:w-[340px] rounded-md bg-slate-950 p-4">
             <code className="text-white">{JSON.stringify(data, null, 2)}</code>
           </pre>
         ),
